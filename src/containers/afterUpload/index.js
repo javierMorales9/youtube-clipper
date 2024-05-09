9bcb8c66a35dc3ace767c6e01b9e8ab8781245bb
Javier Morales de Vera <javi@trackey.io>
2024-05-09 20:27:48 +0200

    updating the batch job container to interact with s3

--- src/containers/afterUpload/index.js
+++ src/containers/afterUpload/index.js
@@ -1,10 +1,42 @@
+const { S3, UploadPartCommand } = require("@aws-sdk/client-s3");
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 async function ls() {
+  console.log('the env', process.env);
+
   const { stdout, stderr } = await exec('ffmpeg -version');
 
-  console.log('the env', process.env);
+  const s3 = new S3({
+    region: process.env.AWS_REGION,
+  });
+
+  const params = {
+    Bucket: process.env.INPUT_BUCKET,
+    Key: process.env.INPUT_KEY
+  };
+
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.log('error', err);
+    }
+    else {
+      console.log('data', data);
+    }
+  });
+
+  s3.putObject({
+    Bucket: process.env.OUTPUT_BUCKET,
+    Key: process.env.INPUT_KEY,
+    Body: 'Hello World!'
+  }, (err, data) => {
+    if (err) {
+      console.log('error', err);
+    }
+    else {
+      console.log('data', data);
+    }
+  });
 }
 
 ls();
