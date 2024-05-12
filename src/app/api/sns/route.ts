0f09bbb8fda93c51add2dba18a0183dc6ff41ba1
Javier Morales de Vera <javi@trackey.io>
2024-05-12 11:24:20 +0200

    fix some s3 issues, make the app runner public, give app runner permissions to create s3 objects and create an endpoint for sns

---
+++ src/app/api/sns/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+const handler = (req: NextRequest) => {
+  console.log('a new sns message', req);
+
+  return NextResponse.json({ message: "Hello, World!" });
+}
+
+export { handler as GET, handler as POST, handler as PUT };
